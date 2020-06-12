60b4b3365d57c9810a5a067a32582bee9000d022
Windwill <jojoeza10@gmail.com>
2020-06-12 17:36:41 +0700

    update code test reddot

--- Test/unit/Red dot Laser/Red dot Laser.ino
+++ Test/unit/Red dot Laser/Red dot Laser.ino
@@ -7,6 +7,6 @@ void setup() {
 void loop() {
 	digitalWrite(laserPin, HIGH); // Open the laser head
 	delay(1000); // Delay one second
-	/*/digitalWrite(laserPin, LOW); // Close the laser head
-	delay(1000); */
+	digitalWrite(laserPin, LOW); // Close the laser head
+	delay(1000); 
 }
