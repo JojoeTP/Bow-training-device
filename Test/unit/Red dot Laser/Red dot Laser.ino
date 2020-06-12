25ee82ddcbc4797e97c51da7036ad4c3f7d0cf46
Windwill <jojoeza10@gmail.com>
2020-06-12 17:32:09 +0700

    test code Red dot

--- Test/unit/Red dot Laser/Red dot Laser.ino
+++ Test/unit/Red dot Laser/Red dot Laser.ino
@@ -1,7 +1,12 @@
-void setup(){
+int laserPin = 13;
 
+void setup() {                
+	pinMode(laserPin, OUTPUT);  // Define the digital output interface pin 13 
 }
 
-void loop(){
-    
+void loop() {
+	digitalWrite(laserPin, HIGH); // Open the laser head
+	delay(1000); // Delay one second
+	/*/digitalWrite(laserPin, LOW); // Close the laser head
+	delay(1000); */
 }
