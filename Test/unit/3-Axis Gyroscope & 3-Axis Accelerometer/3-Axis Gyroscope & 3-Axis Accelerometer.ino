cd92f8d73be6bf7cfbd66801719a0427fac1e6f4
Windwill <jojoeza10@gmail.com>
2020-06-14 03:31:15 +0700

    upload 3-Axis and lib

--- Test/unit/3-Axis Gyroscope & 3-Axis Accelerometer/3-Axis Gyroscope & 3-Axis Accelerometer.ino
+++ Test/unit/3-Axis Gyroscope & 3-Axis Accelerometer/3-Axis Gyroscope & 3-Axis Accelerometer.ino
@@ -1,7 +1,36 @@
-void setup(){
+#include <I2Cdev.h>
+#include <MPU6050.h>
+#include <Wire.h>
+MPU6050 mpu;
+int16_t ax, ay, az;
+int16_t gx, gy, gz;
+int valx , valy , valz;
+char rd;
+int prevVal;
+int led = 13 ;
+int pin11 = 11 , pin10 = 10 ;
+int val1 , val2 ;
+int valgy1 = 0 , valgy2 = 0;
 
+void setup() {
+    pinMode(led,OUTPUT) ;
+    Wire.begin();
+    Serial.begin(9600);
+    Serial.println("Initialize MPU");
+    mpu.initialize();
+    Serial.println(mpu.testConnection() ? "Connected" : "Connection failed");
 }
 
-void loop(){
-    
+void loop() {
+    mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+    valx = map(ax, -17000, 17000, 0, 179);
+    valy = map(ay, -17000, 17000, 0, 179);
+    valz = map(az, -17000, 17000, 0, 179);
+    Serial.print("axis x = ") ; 
+    Serial.print(valx) ; 
+    Serial.print(" axis y = ") ; 
+    Serial.print(valy) ; 
+    Serial.print(" axis z = ") ; 
+    Serial.println(valz) ; 
+    delay(100);
 }
