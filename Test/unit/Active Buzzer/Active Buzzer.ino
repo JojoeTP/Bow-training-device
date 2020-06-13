7639d85efd792802c9bc951ed64e205d8bfc1d9c
Windwill <jojoeza10@gmail.com>
2020-06-14 04:23:24 +0700

    Update code Active Buzzer

--- Test/unit/Active Buzzer/Active Buzzer.ino
+++ Test/unit/Active Buzzer/Active Buzzer.ino
@@ -1,7 +1,16 @@
-void setup(){
-
+int speakerPin = A0;
+int volume = 0;
+void setup () {
+    pinMode (speakerPin, OUTPUT);
+    Serial.begin(9600);
 }
-
-void loop(){
-    
+void loop () {
+  /*for(volume; volume<255; volume++){
+    analogWrite(speakerPin, volume);
+    Serial.println(volume);
+    delay (100);
+  }//analogWrite(speakerPin, 128);*/
+  digitalWrite(speakerPin,1);
+  delay (100);
+  
 }
