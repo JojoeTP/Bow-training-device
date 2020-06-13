ef71512b8ee79612213d530f7c23fa496634fd1e
Windwill <jojoeza10@gmail.com>
2020-06-14 03:45:02 +0700

    Update unkonwn code?

--- Test/unit/3-Axis Gyroscope & 3-Axis Accelerometer/3-Axis Gyroscope & 3-Axis Accelerometer.ino
+++ Test/unit/3-Axis Gyroscope & 3-Axis Accelerometer/3-Axis Gyroscope & 3-Axis Accelerometer.ino
@@ -5,15 +5,15 @@ MPU6050 mpu;
 int16_t ax, ay, az;
 int16_t gx, gy, gz;
 int valx , valy , valz;
-char rd;
-int prevVal;
-int led = 13 ;
-int pin11 = 11 , pin10 = 10 ;
-int val1 , val2 ;
-int valgy1 = 0 , valgy2 = 0;
+//char rd;
+//int prevVal;
+//int led = 13 ;
+//int pin11 = 11 , pin10 = 10 ;
+//int val1 , val2 ;
+//int valgy1 = 0 , valgy2 = 0;
 
 void setup() {
-    pinMode(led,OUTPUT) ;
+    //pinMode(led,OUTPUT) ;
     Wire.begin();
     Serial.begin(9600);
     Serial.println("Initialize MPU");
