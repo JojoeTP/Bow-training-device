42bfb5176ae13180c31715c22ccdaadbe5d68376
Windwill <jojoeza10@gmail.com>
2020-06-15 04:39:14 +0700

    Update code 7 segment

--- Test/unit/WiFi Ethernet Module/WiFi Ethernet Module.ino
+++ Test/unit/WiFi Ethernet Module/WiFi Ethernet Module.ino
@@ -1,7 +1,26 @@
-void setup(){
+int ch_pd = 3;
+
+int io0 = 2;
+
+void setup() {
+
+pinMode(ch_pd, OUTPUT);
+
+pinMode(io0, OUTPUT);
+
+digitalWrite(io0,LOW);
+
+digitalWrite(ch_pd,LOW);
+
+delay(1000);
+
+digitalWrite(ch_pd, HIGH);
 
 }
 
-void loop(){
-    
-}
+void loop()
+
+{
+
+ 
+
