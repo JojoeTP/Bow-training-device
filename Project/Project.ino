dd505be5283f242eb746ec481435e59c08ac57da
Windwill <jojoeza10@gmail.com>
2020-06-15 12:51:39 +0700

    Update code wifi

---
+++ Project/Project.ino
@@ -0,0 +1,8 @@
+#define ESP8266_BAUD 9600
+
+void setup() {
+    
+}
+void loop() {
+  
+}
--- Test/unit/WiFi Ethernet Module/WiFi Ethernet Module.ino
+++ Test/unit/WiFi Ethernet Module/WiFi Ethernet Module.ino
@@ -1,22 +1,23 @@
 #define BLYNK_PRINT Serial
 #include <ESP8266_Lib.h>
 #include <BlynkSimpleShieldEsp8266.h>
-
+#include <BlynkApiArduino.h>
 // You should get Auth Token in the Blynk App.
 // Go to the Project Settings (nut icon).
-char auth[] = "yourBlynkToken";
+char auth[] = "t_S8iNAWV6BNB6_K9_ybEKWRkls3xxR3";
 
 // Your WiFi credentials.
 // Set password to "" for open networks.
-char ssid[] = "yourSSID";
-char pass[] = "yourPassword";
+char ssid[] = "Homechill";
+char pass[] = "Homechill117";
 
 
-#include <SoftwareSerial>
-SoftwareSerial EspSerial(2, 3); // RX, TX
+#include <SoftwareSerial.h>
+SoftwareSerial EspSerial(0, 1); // RX, TX
 
 // Your ESP8266 baud rate:
 #define ESP8266_BAUD 9600
+
 
 ESP8266 wifi(&EspSerial);
 
@@ -36,3 +37,4 @@ void loop()
 {
   Blynk.run();
 }
+
