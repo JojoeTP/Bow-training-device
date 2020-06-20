b919c4aa5ff2b450da27d5e68f3a33955e398b5a
Windwill <jojoeza10@gmail.com>
2020-06-21 04:08:56 +0700

    copy code from P jeeno

--- Test/unit/WiFi Ethernet Module/WiFi Ethernet Module.ino
+++ Test/unit/WiFi Ethernet Module/WiFi Ethernet Module.ino
@@ -1,40 +1,30 @@
 #define BLYNK_PRINT Serial
 #include <ESP8266_Lib.h>
 #include <BlynkSimpleShieldEsp8266.h>
-#include <BlynkApiArduino.h>
-// You should get Auth Token in the Blynk App.
-// Go to the Project Settings (nut icon).
-char auth[] = "t_S8iNAWV6BNB6_K9_ybEKWRkls3xxR3";
 
-// Your WiFi credentials.
-// Set password to "" for open networks.
-char ssid[] = "Mi Phone";
-char pass[] = "0123456789";
+char auth[] = "z8JkTw_tZJO0rDq6rFmUAkQn8WpS4z0-";
 
+char ssid[] = "Homechill";
+char pass[] = "Homechill117";
 
 #include <SoftwareSerial.h>
-SoftwareSerial EspSerial(2, 3); // RX, TX  ฟ้า--pin2is(Tx)++Rx  เทา--pin3is(Rx)++Tx   
+SoftwareSerial EspSerial(3, 2); // RX, TX
 
-// Your ESP8266 baud rate:
-#define ESP8266_BAUD 9600
-
-
+#define ESP8266_BAUD 38400
 ESP8266 wifi(&EspSerial);
 
-void setup()
-{
-  // Debug console
+WidgetTerminal Terminal(V0);
+
+void setup() {
   Serial.begin(9600);
 
-  // Set ESP8266 baud rate
   EspSerial.begin(ESP8266_BAUD);
   delay(10);
 
   Blynk.begin(auth, wifi, ssid, pass);
-}
 
-void loop()
-{
-  Blynk.run();
+  Terminal.clear();
+  Terminal.println("Wifi Connected");
+  Terminal.flush();
 }
 
